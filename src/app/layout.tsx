58f9bef1171aeccf7ec9209af0eaf2baf5c2e4f9
Will Pulier <37934059+wpulier@users.noreply.github.com>
2025-06-05 02:28:16 -0400

    Remove Google font dependency

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
 import "./globals.css";
 import Footer from "./components/Footer";
 import Navigation from "./components/Navigation";
 import LegendaryCursorProvider from "@/components/LegendaryCursorProvider";
-
-const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Wild Studios | Design for the Digital Age",
@@ -19,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${inter.className} bg-white bg-[radial-gradient(#f0f0f0_1px,transparent_1px)] bg-[length:20px_20px]`}>
+      <body className="bg-white bg-[radial-gradient(#f0f0f0_1px,transparent_1px)] bg-[length:20px_20px]">
         <div className="flex flex-col min-h-screen">
           <Navigation />
           <main className="flex-grow mb-6">
